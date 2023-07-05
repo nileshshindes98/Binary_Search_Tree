efe6ced156fc820838983f54ca428ab973075808
Nilesh Shinde <nileshshindes98@gmail.com>
2023-07-05 23:51:54 +0530

    Added : Welcome msg on master branch

---
+++ BinarySearchTree.java
@@ -0,0 +1,7 @@
+package binarySearchTree;
+
+public class BinarySearchTree {
+    public static void main(String[] args) {
+        System.out.println("-----Binary Search Tree (BST)-----");
+    }
+}
